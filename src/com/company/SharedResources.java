322d5a15418db68dc5b8294821f17cd1334ae99a
Bryan <bryan.dov.bergman@gmail.com>
2020-05-11 21:07:58 +0300

    Initial commit

---
+++ src/com/company/SharedResources.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class SharedResources {
+
+    private Worker owner;
+
+    public SharedResources(Worker owner) {
+        this.owner = owner;
+    }
+
+    public Worker getOwner() {
+        return owner;
+    }
+
+    public synchronized void setOwner(Worker owner) {
+        this.owner = owner;
+    }
+}
